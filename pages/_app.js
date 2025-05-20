bb44c141fc6c0607b38716496e497f893bb40789
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-05-20 22:06:55 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../public/styles/main.css';
+import 'highlight.js/styles/github.css'; // Syntax highlighting theme
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
